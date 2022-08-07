12c7b4ff50283654c9f6a549f6700ffd944e0884
Tumi <oluwatumininuojo25@gmail.com>
2022-08-07 14:47:23 +0100

    initial commit

--- app/src/main/java/com/tumininu/movielist/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.tumininu.movielist.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.tumininu.movielist.R
+import androidx.lifecycle.ViewModelProvider
+import com.tumininu.movielist.databinding.ActivityMainBinding
+import com.tumininu.movielist.presentation.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[MainViewModel::class.java]
+
+
     }
 }
