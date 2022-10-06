17fca00f1b1edc05249e40c4377912823f2e76a9
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-10-06 10:53:33 +0100

    ui update

--- app/src/main/java/com/tumininu/movielist/presentation/ui/home/HomeView.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/home/HomeView.kt
@@ -1,17 +1,21 @@
 package com.tumininu.movielist.presentation.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(modifier: Modifier = Modifier) {
     Scaffold(topBar = {
         TopAppBar {
+            Spacer(modifier = modifier.width(8.dp))
             Text(text = "Casestar")
         }
     }) { padding ->
