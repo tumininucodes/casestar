c1c2e4761a1f31e8d4b13c0e2d423e1be445daae
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-10-11 15:56:24 +0100

    ui update

--- app/src/main/java/com/tumininu/movielist/presentation/ui/home/HomeView.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/home/HomeView.kt
@@ -7,18 +7,25 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.tumininu.movielist.presentation.HomeViewModel
 import com.tumininu.movielist.presentation.ui.home.components.MovieView
 
 @Composable
 fun HomeView(modifier: Modifier = Modifier) {
+
+    val viewModel = remember { HomeViewModel() }
+
     Scaffold(topBar = {
         TopAppBar {
             Spacer(modifier = modifier.width(8.dp))
             Text(text = "Casestar")
         }
     }) { padding ->
+        val data = viewModel.getMovies().observe(life)
         Column(modifier = modifier.padding(padding)) {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
--- app/src/main/java/com/tumininu/movielist/presentation/ui/home/components/MovieView.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/home/components/MovieView.kt
@@ -1,17 +1,12 @@
 package com.tumininu.movielist.presentation.ui.home.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,22 +27,16 @@ fun MovieView(modifier: Modifier = Modifier) {
                     .align(Alignment.CenterHorizontally),
                 previewPlaceholder = R.drawable.image,
             )
+            Spacer(modifier = modifier.height(8.dp))
             Text(
                 text = "Movie title",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.subtitle2,
+                fontSize = 11.sp,
+                maxLines = 1,
                 modifier = modifier
                     .align(Alignment.Start)
-                    .padding(8.dp)
+                    .padding(horizontal = 8.dp)
             )
-            Text(
-                text = "Description of the movie",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.subtitle2,
-                modifier = modifier
-                    .align(Alignment.Start)
-                    .padding(8.dp)
-            )
+            Spacer(modifier = modifier.height(8.dp))
         }
     }
 }
