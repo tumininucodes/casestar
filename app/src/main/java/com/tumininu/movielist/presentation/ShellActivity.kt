ed40807943e80d53f7b5ae1a1751e4bccca78d5a
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-10-23 10:03:36 +0100

    development

--- app/src/main/java/com/tumininu/movielist/presentation/ShellActivity.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ShellActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tumininu.movielist.presentation.sharedelements.SharedElementsRoot
 import com.tumininu.movielist.presentation.ui.home.HomeView
 import com.tumininu.movielist.presentation.ui.theme.MovieListTheme
 
