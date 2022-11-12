8c57320263f52b5886cf56fcc2a5194e6a594445
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-11-12 02:53:22 +0100

    development

--- app/src/main/java/com/tumininu/movielist/presentation/ui/search/SearchView.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/search/SearchView.kt
@@ -1,8 +1,11 @@
 package com.tumininu.movielist.presentation.ui.search
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SearchView() {
+    Column {
 
+    }
 }
