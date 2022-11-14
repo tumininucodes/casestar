e9d4f7fcfd1802a6b69beb71680022f2f4030fb6
Oluwatumininu Ojo <oluwatumininuojo25@gmail.com>
2022-11-14 20:41:31 +0100

    development

--- app/src/main/java/com/tumininu/movielist/presentation/ui/search/SearchView.kt
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/search/SearchView.kt
@@ -1,11 +1,20 @@
 package com.tumininu.movielist.presentation.ui.search
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import com.tumininu.movielist.presentation.ui.search.components.Search
 
 @Composable
-fun SearchView() {
+fun SearchView(modifier: Modifier = Modifier) {
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
     Column {
+        Search(textState)
+        Spacer(modifier = modifier)
 
     }
 }
---
+++ app/src/main/java/com/tumininu/movielist/presentation/ui/search/components/Search.kt
@@ -0,0 +1,87 @@
+package com.tumininu.movielist.presentation.ui.search.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tumininu.movielist.presentation.ui.theme.Dark
+import kotlinx.coroutines.Dispatchers
+
+@Composable
+fun Search(state: MutableState<TextFieldValue>) {
+
+    val focusRequester = remember { FocusRequester() }
+
+    TextField(
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+        },
+        modifier = Modifier
+            .focusRequester(focusRequester)
+            .fillMaxWidth(),
+        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        state.value =
+                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.White,
+            cursorColor = Color.White,
+            leadingIconColor = Color.White,
+            trailingIconColor = Color.White,
+            backgroundColor = Dark,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        placeholder = {
+            Text(text = "Search movies")
+        }
+    )
+
+    LaunchedEffect(Dispatchers.Main) {
+        focusRequester.requestFocus()
+    }
+}
